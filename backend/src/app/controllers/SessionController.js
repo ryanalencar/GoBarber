b394e7ed5538323380d0d1a7d0e09cc20b99873b
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2020-06-23 16:40:09 -0300

    feat: user authentication

---
+++ backend/src/app/controllers/SessionController.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+
+import authConfig from '../../config/auth';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { expiresIn, secret } = authConfig;
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await (await user).checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, secret, { expiresIn }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8564005643cc41d63b4c695c38c826a3',
+  expiresIn: '7d',
+};
