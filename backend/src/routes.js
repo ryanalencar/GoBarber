aad5b704f7233b8fa637c93db645e713ea3266e3
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2020-06-23 20:08:05 -0300

    feat: auth middleware

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
+// users
 routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
+
+// sessions
 routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
