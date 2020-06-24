0cc69764a0d57fcea13c4aedee1b5c7aa8337b52
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2020-06-24 00:04:08 -0300

    feat: user update function

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -2,7 +2,9 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const userExists = await User.findOne({ where: { email: req.body.email } });
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
@@ -22,8 +24,33 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    const { userId } = req;
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
