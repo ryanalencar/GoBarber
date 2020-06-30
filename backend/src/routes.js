59b60c0490012ddc98df64ca26c77fe2dce6161a
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2020-06-30 14:13:58 -0300

    feat: get providers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,17 +7,18 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerCfg);
 
-// users
 routes.post('/users', UserController.store);
 routes.put('/users', authMiddleware, UserController.update);
 
-// sessions
 routes.post('/sessions', SessionController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', authMiddleware, ProviderController.index);
 
 module.exports = routes;
