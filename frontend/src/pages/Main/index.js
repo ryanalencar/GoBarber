db2bd801bf862f6d1f54d908b413f97d3185849e
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2020-09-03 15:49:00 -0300

    feat: styled-components

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Title } from './styles';
+
 function Main() {
-  return <h1>Main Page</h1>;
+  return (
+    <Title error>
+      Main Page
+      <small>main</small>
+    </Title>
+  );
 }
 
 export default Main;
