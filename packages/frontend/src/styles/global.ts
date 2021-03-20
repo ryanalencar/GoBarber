7884b448c867aa0853e265b325ec49968512c266
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-03-20 15:30:09 -0300

    refactor: global styles

--- packages/frontend/src/styles/global.ts
+++ packages/frontend/src/styles/global.ts
@@ -6,4 +6,32 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  *:focus {
+    outline:0;
+  }
+
+  html, body {
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
 `
