cdf3fb69534d0f8185981a42facba171f25a1b94
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-04-08 17:20:45 -0300

    feat: redux persist auth and user

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -2,19 +2,22 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 // import '~/config/Reactotron'
 import GlobalStyle from '~/styles/global'
 import theme from '~/styles/theme'
-import store from '~/store'
+import { store, persistor } from '~/store'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-        <GlobalStyle />
-      </ThemeProvider>
+      <PersistGate persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+          <GlobalStyle />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   )
 }
--- packages/frontend/src/store/index.ts
+++ packages/frontend/src/store/index.ts
@@ -1,16 +1,19 @@
 import createSagaMiddleware from 'redux-saga'
+import { persistStore } from 'redux-persist'
 
 import createStore from './createStore'
 import rootReducer from './reducers'
 import rootSaga from './sagas'
+import persistReducers from './persistReducers'
 
 // const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null
 const sagaMiddleware = createSagaMiddleware()
 
 const middlewares = [sagaMiddleware]
 
-const store = createStore(rootReducer, middlewares)
+const store = createStore(persistReducers(rootReducer), middlewares)
+const persistor = persistStore(store)
 
 sagaMiddleware.run(rootSaga)
 
-export default store
+export { store, persistor }
