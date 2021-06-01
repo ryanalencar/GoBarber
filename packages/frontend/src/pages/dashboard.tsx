36cfd83594fbc900a6f1ed156f7a92e4533e963a
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-06-01 08:17:26 -0300

    feat: dashboard styled

--- packages/frontend/src/pages/dashboard.tsx
+++ packages/frontend/src/pages/dashboard.tsx
@@ -1,10 +1,101 @@
 import React from 'react'
+import styled, { DefaultTheme } from 'styled-components'
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import PageLayout from '~/components/layouts'
+
+interface IStyledProps {
+  theme?: DefaultTheme
+  available?: boolean
+  past?: boolean
+}
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-self: center;
+    align-items: center;
+
+    button {
+      border: 0;
+      background: none;
+    }
+
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    margin-top: 30px;
+  }
+`
+
+const Time = styled.li`
+  padding: 20px;
+  border-radius: 4px;
+  background: #fff;
+
+  opacity: ${({ past }) => (past ? 0.6 : 1)};
+
+  strong {
+    display: block;
+    color: ${({ theme, available }: IStyledProps) =>
+      available ? '#666' : theme.colors.rocketPurple};
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    color: ${({ available }) => (available ? '#999' : '#666')};
+  }
+`
 
 const Dashboard: React.FC = () => {
   return (
     <PageLayout>
-      <h1>Dashboard Page</h1>
+      <Container>
+        <header>
+          <button type="button">
+            <MdChevronLeft color="#fff" size={32} />
+          </button>
+          <strong>01 de Junho</strong>
+          <button type="button">
+            <MdChevronRight color="#fff" size={32} />
+          </button>
+        </header>
+
+        <ul>
+          <Time past>
+            <strong>08:00</strong>
+            <span>Diego Fernandes</span>
+          </Time>
+          <Time available>
+            <strong>09:00</strong>
+            <span>Em aberto</span>
+          </Time>
+          <Time>
+            <strong>10:00</strong>
+            <span>Em aberto</span>
+          </Time>
+          <Time>
+            <strong>11:00</strong>
+            <span>Em aberto</span>
+          </Time>
+        </ul>
+      </Container>
     </PageLayout>
   )
 }
